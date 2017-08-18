336c21db34b2dca115124c775b69502b5ad2bd4d
lihangogo <30977458+lihangogo@users.noreply.github.com>
2017-08-18 11:40:46 +0800

    a

--- LHweather/gen/com/example/lhweather/R.java
+++ LHweather/gen/com/example/lhweather/R.java
@@ -72,7 +72,8 @@ public final class R {
         public static final int p999=0x7f020034;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int layout1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -85,6 +86,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int image_error=0x7f050003;
+        public static final int key=0x7f050004;
     }
     public static final class style {
         /** 
